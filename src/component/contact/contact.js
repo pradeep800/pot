c4014c36faaf3940d4b5b1248b2a68e9419384a1
pradeep <pradeep8b0@gmail.com>
2022-04-08 23:45:12 +0530

    new

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,22 +1,68 @@
 import gmail from "./gmail.png";
-import lindin from "./linkdin.png";
+import lindin from "./linkedin.png";
 import twitter from "./twitter.png";
 import github from "./github.png";
 export default function Contact() {
   return (
-    <div className="flex h-[80vh] items-center  justify-around">
-      <a href="mailto:pradeep8b0@gmail.com">
-        <img alt="gmail photo" className="w-[100px]" src={gmail} />
-      </a>
-      <a href="https://www.linkedin.com/in/pradeep-b-6554a41b0" target="_blank">
-        <img alt="linkdin" className="w-[100px]" src={lindin} />
-      </a>
-      <a href="https://twitter.com/pradeep14880712" target="_blank">
-        <img alt="twitter" className="w-[100px]" src={twitter} />
-      </a>
-      <a href="https://github.com/pradeep800" target="_blank">
-        <img alt="github" className="w-[100px]" src={github} />
-      </a>
+    <div className="flex h-[100vh] items-center  justify-center bg-green-200 ">
+      <div className="bg-green-300 h-[70vh] w-[90vw]  flex flex-col  justify-center content-around rounded-2xl">
+        <div className=" font-cool font-black text-4xl flex  flex-col items-center  justify-center">
+          Contact Me
+        </div>
+        <div className="pt-[10vh] flex  justify-around items-center ">
+          <a href="mailto:pradeep8b0@gmail.com">
+            <img
+              alt="gmail photo"
+              className="w-[50px] sm:w-[100px] animate-shadowChange"
+              src={gmail}
+              style={{
+                borderRadius: "30px",
+                backgroundColor: "transparent",
+                boxShadow: "4px 5px 30px blue",
+              }}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/pradeep-b-6554a41b0"
+            target="_blank"
+          >
+            <img
+              alt="linkdin"
+              className="shadow-2xl shadow-blue-700 animate-shadowChange bg-blue-200 w-[50px] sm:w-[100px] "
+              src={lindin}
+              style={{
+                borderRadius: "30px",
+                backgroundColor: "transparent",
+                boxShadow: "4px 5px 30px blue",
+              }}
+            />
+          </a>
+          <a href="https://twitter.com/pradeep14880712" target="_blank">
+            <img
+              style={{
+                borderRadius: "30px",
+                backgroundColor: "transparent",
+                boxShadow: "4px 5px 30px blue",
+              }}
+              alt="twitter"
+              className=" w-[50px] sm:w-[100px]  animate-shadowChange"
+              src={twitter}
+            />
+          </a>
+          <a href="https://github.com/pradeep800" target="_blank">
+            <img
+              style={{
+                borderRadius: "30px",
+                backgroundColor: "transparent",
+                boxShadow: "4px 5px 30px blue",
+              }}
+              alt="github"
+              className="w-[50px] sm:w-[100px] animate-shadowChange"
+              src={github}
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,27 @@ module.exports = {
     extend: {
       fontFamily: {
         red: ["Red Hat Mono"],
+        cool: ["Rubik Bubbles"],
       },
       animation: {
         uptodown: "uptodown 1s ease-out  normal forwards",
+        shadowChange: "shadowChange 2s ease-in alternate infinite",
       },
       keyframes: {
+        shadowChange: {
+          "0%": {
+            boxShadow: "0px 0px 0px",
+          },
+          "50%": {
+            boxShadow: "6px 8px 5px green.-6px -8px 15px green",
+          },
+          "75%": {
+            boxShadow: "7px 9px 20px green,-7px -9px 20px green",
+          },
+          "100%": {
+            boxShadow: "8px 10px 25px green,-8px -10px 25px green",
+          },
+        },
         uptodown: {
           "0%": {
             top: "0px",
