b8c7b512831abecb342ed6b569655ea514dbd5a9
pradeep <pradeep8b0@gmail.com>
2022-04-09 07:16:08 +0530

    new

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -34,7 +34,7 @@ export default function About() {
           ></img>
           <img
             src={joint}
-            className="absolute w-[200px] left-[0px]  top-[95px] sm:animate-joint2 animate-joint lg:animate-joint3"
+            className="absolute w-[200px] left-[0px]  top-[95px] animate-joint sm:animate-joint2 lg:animate-joint3"
           ></img>
           <p
             id="inpo"
