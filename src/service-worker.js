058fd5de6d7f551a871e0b8e33f368a7ba8f8aae
pradeep <pradeep8b0@gmail.com>
2022-04-17 22:49:37 +0530

    new

--- src/service-worker.js
+++ src/service-worker.js
@@ -64,7 +64,7 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-const version = 2;
+const version = 21;
 let allfile = `all-file-${version}`;
 self.addEventListener("message", (event) => {});
 self.addEventListener("install", (ev) => {
